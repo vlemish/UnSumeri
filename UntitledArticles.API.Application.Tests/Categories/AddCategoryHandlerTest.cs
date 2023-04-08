aa2cb1ecd7230b60894cc74ff23ef42e86389cbb
Vladislav Lemish <vladislav.lemish@dataart.com>
2023-04-08 23:55:37 +0300

    Cover existed commands/queries with unit tests

---
+++ UntitledArticles.API.Application.Tests/Categories/AddCategoryHandlerTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using FluentValidation.Results;
+using Microsoft.Extensions.Logging;
+using Moq;
+using UntitiledArticles.API.Application.Categories.Commands.Add;
+using UntitiledArticles.API.Application.OperationStatuses;
+using UntitledArticles.API.Domain.Contracts;
+using UntitledArticles.API.Domain.Entities;
+
+namespace UntitledArticles.API.Application.Tests.Categories;
+
+public class AddCategoryHandlerTest
+{
+    private Mock<ICategoryRepository> _categoryRepository;
+
+    [Theory]
+    [InlineData("testName")]
+    [InlineData("t")]
+    public void TestAddCategoryValidator_WhenNameNotNullOrEmpty_ThenValid(string name)
+    {
+        int expectedErrorsCount = 0;
+        AddCategory addCategory = new(name);
+        
+        AddCategoryValidator validator = new();
+        ValidationResult validationResult = validator.Validate(addCategory);
+        
+        Assert.True(validationResult.IsValid);
+        Assert.Equal(expectedErrorsCount, validationResult.Errors.Count);
+    }
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void TestAddCategoryValidator_WhenNameNullOrEmpty_ThenValid(string name)
+    {
+        int expectedErrorsCount = 1;
+        AddCategory addCategory = new(name);
+        
+        AddCategoryValidator validator = new();
+        ValidationResult validationResult = validator.Validate(addCategory);
+        
+        Assert.False(validationResult.IsValid);
+        Assert.Equal(expectedErrorsCount, validationResult.Errors.Count);
+    }
+    
+    [Fact]
+    public async Task TestAddCategoryHandler_WhenCategoryAdded_ThenSuccessStatus()
+    {
+        string name = "test_name";
+        AddCategory request = new AddCategory(name);
+        OperationStatusValue expectedOperationStatus = OperationStatusValue.OK;
+        Category addedCategory = new Category()
+        {
+            Id = 2, Articles = null, Name = name
+        };
+
+        SetupMocks(addedCategory);
+        
+        AddCategoryHandler handler = new(new Mock<ILogger<AddCategoryHandler>>().Object, _categoryRepository.Object);
+
+        AddCategoryResponse actual = await handler.Handle(request, default);
+        
+        Assert.NotNull(actual);
+        Assert.Equal(expectedOperationStatus, actual.Status.Status);
+        Assert.Equal(actual.Result.Id, addedCategory.Id);
+        
+        _categoryRepository
+            .Verify(m => m.AddAsync(It.IsAny<Category>()), Times.Once());
+    }
+    
+    private void SetupMocks(Category addedCategory)
+    {
+        _categoryRepository = new();
+        _categoryRepository
+            .Setup(m => m.AddAsync(It.IsAny<Category>()))
+            .ReturnsAsync(addedCategory);
+    }
+}
---
+++ UntitledArticles.API.Application.Tests/Categories/GetCategoryHandlerTest.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using UntitiledArticles.API.Application.Categories.Queries;
+using UntitiledArticles.API.Application.OperationStatuses;
+using UntitledArticles.API.Domain.Contracts;
+using UntitledArticles.API.Domain.Entities;
+
+namespace UntitledArticles.API.Application.Tests.Categories;
+
+public class GetCategoryHandlerTest
+{
+    private Mock<ICategoryRepository> _categoryRepositoryMock;
+
+    [Fact]
+    public async Task TestGetCategoryHandler_WhenCategoryExist_ThenSuccess()
+    {
+        int id = 2;
+        List<Category> subCategories = CreateSubCategories(id, 3);
+        OperationStatusValue expectedOperationValue = OperationStatusValue.OK;
+        Category category = new()
+        {
+            Id = id,
+            Name = "test_category",
+            SubCategories = subCategories,
+        };
+        GetCategory request = new(id);
+
+        SetupMocks(category);
+
+        GetCategoryHandler handler = new(new Mock<ILogger<GetCategoryHandler>>().Object,
+            _categoryRepositoryMock.Object);
+
+        GetCategoryResponse result = await handler.Handle(request, default);
+
+        Assert.Equal(expectedOperationValue, result.Status.Status);
+        Assert.True(IsCategoryResultEqualToCategory(category, result.Result));
+    }
+    
+    [Fact]
+    public async Task TestGetCategoryHandler_WhenCategoryNotExist_ThenNotFound()
+    {
+        int id = 2;
+        Category category = null;
+        OperationStatusValue expectedOperationValue = OperationStatusValue.NotFound;
+        GetCategory request = new(id);
+
+        SetupMocks(category);
+
+        GetCategoryHandler handler = new(new Mock<ILogger<GetCategoryHandler>>().Object,
+            _categoryRepositoryMock.Object);
+
+        GetCategoryResponse result = await handler.Handle(request, default);
+
+        Assert.Equal(expectedOperationValue, result.Status.Status);
+    }
+
+    private void SetupMocks(Category category)
+    {
+        _categoryRepositoryMock = new();
+        _categoryRepositoryMock
+            .Setup(m => m.GetOneByFilter(It.IsAny<Func<Category, bool>>()))
+            .ReturnsAsync(category);
+    }
+    
+    private bool IsCategoryResultEqualToCategory(Category category, GetCategoryResult result)
+    {
+        if (category.Id == result.Id || category.Name == result.Name || category.ParentId == result.ParentId ||
+            category.SubCategories.Count == category.SubCategories.Count)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private List<Category> CreateSubCategories(int id, int numberOfSubcategories)
+    {
+        List<Category> subCategories = new();
+        for (int i = 0; i < numberOfSubcategories; i++)
+        {
+            subCategories.Add(new()
+            {
+                Name = $"name{i + 1}",
+                ParentId = id,
+            });
+        }
+
+        return subCategories;
+    }
+}
