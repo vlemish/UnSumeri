f29f5d2b7629d390abdeeb7ca5b50881979c3520
Vladislav Lemish <vladislav.lemish@dataart.com>
2023-04-17 18:49:34 +0300

    Add endpoint for deletion
    Improve DAL and other commands
    
    Fix Fluent Validation

---
+++ UntitiledArticles.API.Application/Categories/Commands/Delete/Statuses/DeleteCategoryNotFound.cs
@@ -0,0 +1,16 @@
+﻿using UntitiledArticles.API.Application.OperationStatuses;
+
+namespace UntitiledArticles.API.Application.Categories.Commands.Delete.Statuses;
+
+public class DeleteCategoryNotFoundStatus : IOperationStatus
+{
+    private readonly int _id;
+
+    public DeleteCategoryNotFoundStatus(int id)
+    {
+        _id = id;
+    }
+
+    public OperationStatusValue Status { get; } = OperationStatusValue.NotFound;
+    public string Message { get; } = $"Category when Category Id = {_id} doesn't exist!";
+}
---
+++ UntitiledArticles.API.Application/Categories/Commands/Delete/Statuses/DeleteCategorySuccess.cs
@@ -0,0 +1,16 @@
+﻿using UntitiledArticles.API.Application.OperationStatuses;
+
+namespace UntitiledArticles.API.Application.Categories.Commands.Delete.Statuses;
+
+public class DeleteCategorySuccessStatus : IOperationStatus
+{
+    private readonly int _id;
+    
+    public DeleteCategorySuccessStatus(int id)
+    {
+        _id = id;
+    }
+
+    public OperationStatusValue Status { get; } = OperationStatusValue.OK;
+    public string Message { get; } = $"Category when Category Id = {_id} was successfully deleted!";
+}
