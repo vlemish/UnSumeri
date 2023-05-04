0ac6a0f7de6a1c9fec515fffe7e1a58cb2f9683a
vlemish <vlad.lemish123@gmail.com>
2023-05-04 22:39:15 +0300

    WIP: 63963f3 Merge pull request #21 from vlemish/20-implement-endpoint-for-deleting-a-category

--- UntitledArticles.API.Infrastructure/Extensions/QueryExtensions.cs
+++ UntitledArticles.API.Infrastructure/Extensions/QueryExtensions.cs
@@ -2,26 +2,49 @@
 using Microsoft.EntityFrameworkCore.Query;
 
 using System.Linq.Expressions;
-
-using UntitledArticles.API.Domain.Enums;
+using System.Text;
 
 namespace UntitledArticles.API.Infrastructure.Extensions
 {
     internal static class QueryExtensions
     {
-        internal static IIncludableQueryable<TInput, ICollection<TInput>> IncludeSelfReferencingCollectionWithDepth<TInput>(this DbSet<TInput> dbSet,
-            Expression<Func<TInput, ICollection<TInput>>> navigationInclude,
-            int depth)
-            where TInput : class
-        {
-            var includableQueryale = dbSet.Include(navigationInclude);
-            for (int i = 0; i < depth - 1; i++)
-            {
-                includableQueryale = includableQueryale.ThenInclude(navigationInclude);
-            }
+        // internal static IIncludableQueryable<TInput, ICollection<TInput>> IncludeSelfReferencingCollectionWithDepth<TInput>(this DbSet<TInput> dbSet,
+        //     Expression<Func<TInput, ICollection<TInput>>> navigationInclude,
+        //     int depth)
+        //     where TInput : class
+        // {
+        //     var includableQueryale = dbSet.Include(navigationInclude);
+        //     for (int i = 0; i < depth - 1; i++)
+        //     {
+        //         includableQueryale = includableQueryale.ThenInclude(navigationInclude);
+        //     }
 
-            return includableQueryale;
-        }
+        //     return includableQueryale;
+        // }
+
+    //     internal static IQueryable<TEntity> Include<TEntity>(this IQueryable<TEntity> source,
+    //   int levelIndex, Expression<Func<TEntity, TEntity>> expression)
+    //   {
+    //     if (levelIndex < 0)
+    //     {
+    //         throw new ArgumentOutOfRangeException(nameof(levelIndex));
+    //     }
+        
+    //     var member = (MemberExpression)expression.Body;
+    //     var property = member.Member.Name;
+    //     var sb = new StringBuilder();
+    //     for (int i = 0; i < levelIndex; i++)
+    //     {
+    //         if (i > 0)
+    //         {
+    //             sb.Append(Type.Delimiter);
+    //         }
+
+    //     sb.Append(property);
+    //   }
+
+    //   return source.Include("Path");
+    // }
 
         //internal static IIncludableQueryable<TInput, ICollection<TInput>> IncludeSelfReferencingCollectionWithDepth<TInput>(this IQueryable<TInput> input,
         //    Expression<Func<TInput, ICollection<TInput>>> navigationInclude,
@@ -38,16 +61,16 @@ namespace UntitledArticles.API.Infrastructure.Extensions
         //}
 
 
-        internal static IOrderedQueryable<T> Sort<T, TKey>(this DbSet<T> dbSet, Expression<Func<T, TKey>> keySelector,
-            OrderByOption orderByOption)
-            where T : class
-        {
-            if (orderByOption == OrderByOption.ASC)
-            {
-                return dbSet.OrderBy(keySelector);
-            }
+        // internal static IOrderedQueryable<T> Sort<T, TKey>(this DbSet<T> dbSet, Expression<Func<T, TKey>> keySelector,
+        //     OrderByOption orderByOption)
+        //     where T : class
+        // {
+        //     if (orderByOption == OrderByOption.ASC)
+        //     {
+        //         return dbSet.OrderBy(keySelector);
+        //     }
 
-            return dbSet.OrderBy(keySelector);
-        }
+        //     return dbSet.OrderBy(keySelector);
+        // }
     }
 }
