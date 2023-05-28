59fdb48f25459e42e089953bab2c8c4e08b2e40d
vlemish <vlad.lemish123@gmail.com>
2023-05-28 15:20:53 +0300

    Add update article endpoint

---
+++ UntitiledArticles.API.Application/Articles/Commands/Update/Statuses/UpdateArticleSuccess.cs
@@ -0,0 +1,17 @@
+namespace UntitiledArticles.API.Application.Articles.Commands.Update.Statuses;
+
+using UntitiledArticles.API.Application.OperationStatuses;
+
+public class UpdateArticleSuccess : IOperationStatus
+{
+    private readonly int _id;
+
+    public UpdateArticleSuccess(int id)
+    {
+        this._id = id;
+    }
+
+    public OperationStatusValue Status => OperationStatusValue.OK;
+
+    public string Message => $"Article was successfully updated where id = {this._id}!";
+}
